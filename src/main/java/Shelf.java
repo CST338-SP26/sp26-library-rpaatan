30bd3c3d862e0161ab8b7cfb75505c8d614c6409
rpaatan <rhu.paatan@gmail.com>
2026-04-06 14:29:32 -0700

    03: Methods all padded, moving to testing & bug fixes.

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,3 +1,5 @@
+import Utilities.Code;
+
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -48,6 +50,64 @@ public class Shelf {
         this.subject = subject;
     }
 
+    // Methods
+    public int getBookCount(Book book) {
+        if (books.containsKey(book)) {
+            return books.get(book);
+        }
+
+        // if book isn't stored on shelf, return -1.
+        // 0 is a valid index, since the book can be on the shelf, but have all it's copies checked out.
+        return -1;
+    }
+
+    public Code addBook(Book book) {
+        if (books.containsKey(book)) {
+            books.put(book, books.get(book) + 1);
+
+            return Code.SUCCESS;
+        } else if (!books.containsKey(book) && Objects.equals(this.subject, book.getSubject())) {
+            // if the book doesn't exist on the shelf, but the subject of the book we're trying to place exists,
+            // add it to the shelf as a new book.
+
+            books.put(book, 1);
+            System.out.println(book.getTitle() + " added to shelf" + this.toString());
+            // todo: could cause an error, this.toString is greyed out.
+
+            return Code.SUCCESS;
+        }
+
+        // if the program reaches this point, then there was no book that exists on the shelf, or the book's subject didn't match.
+        return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+    }
+
+    public Code removeBook(Book book){
+        if(!books.containsKey(book)) {
+            System.out.println(book.getTitle() + " is not on shelf " + this.subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        } else if (books.containsKey(book) && books.get(book) == 0) {
+            System.out.println("No copies of " + book.getTitle() + " remain on shelf " + this.subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        } else if (books.containsKey(book) && books.get(book) > 0) {
+            System.out.println(book.getTitle() + " successfully removed from shelf " + this.subject);
+        }
+
+        // If somehow we get outside the bounds of the code, then return this error.
+        System.out.println("Other error occured trying to remove " + book.getTitle());
+        return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+    }
+
+    public String listBooks() {
+        StringBuilder sb = new StringBuilder(books.size() + "on shelf: " + toString());
+
+        for(Book book : books.keySet()) {
+            sb.append("\n" + book.getTitle() + " by " + book.getAuthor() + " " + book.getISBN() + books.get(book));
+            // should be the title, plus the author name, plus the book ISBN, and then the amount of books that are available.
+            // books.get(book) should access the value connected to the key
+        }
+
+        return sb.toString();
+    }
 
     // Special Methods
     @Override
